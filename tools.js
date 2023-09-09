62208b5fab3d6282e8a55b407da9ae50ea5c7e17
Ryan Lambe <ryanlambe@hotmail.com>
2023-09-09 01:30:00 -0400

    Updated Design + added new projects

--- tools.js
+++ tools.js
@@ -1,15 +1,8 @@
 
 
 function setVideos(){
-    videos = document.getElementsByClassName('video');
+    videos = document.getElementsByClassName('playable');
     for(const video of videos){
-        //check screen aspect ratio
-        if (window.innerWidth / window.innerHeight < 1.75){
-            video.src = video.children[0].src; // use 16:9 video
-        }
-        else{
-            video.src = video.children[1].src; // use 21:9 video
-        }
         video.load();
         video.play();
     }
